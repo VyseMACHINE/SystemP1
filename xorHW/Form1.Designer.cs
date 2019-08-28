52ad04d27db7010145975d9434451fd4161ddd6a
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2019-08-28 19:24:09 +0600

    Add files via upload

---
+++ xorHW/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace xorHW
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtSourceFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.radCrypt = new System.Windows.Forms.RadioButton();
+            this.radDecrypt = new System.Windows.Forms.RadioButton();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtSourceFile
+            // 
+            this.txtSourceFile.Location = new System.Drawing.Point(106, 12);
+            this.txtSourceFile.Multiline = true;
+            this.txtSourceFile.Name = "txtSourceFile";
+            this.txtSourceFile.Size = new System.Drawing.Size(315, 23);
+            this.txtSourceFile.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Пароль";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(106, 55);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(91, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // radCrypt
+            // 
+            this.radCrypt.AutoSize = true;
+            this.radCrypt.Location = new System.Drawing.Point(212, 58);
+            this.radCrypt.Name = "radCrypt";
+            this.radCrypt.Size = new System.Drawing.Size(95, 17);
+            this.radCrypt.TabIndex = 4;
+            this.radCrypt.TabStop = true;
+            this.radCrypt.Text = "Зашифровать";
+            this.radCrypt.UseVisualStyleBackColor = true;
+            // 
+            // radDecrypt
+            // 
+            this.radDecrypt.AutoSize = true;
+            this.radDecrypt.Location = new System.Drawing.Point(313, 58);
+            this.radDecrypt.Name = "radDecrypt";
+            this.radDecrypt.Size = new System.Drawing.Size(101, 17);
+            this.radDecrypt.TabIndex = 5;
+            this.radDecrypt.TabStop = true;
+            this.radDecrypt.Text = "Расшифровать";
+            this.radDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 95);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(409, 23);
+            this.progressBar.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(255, 135);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Пуск";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(346, 135);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Выйти";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 170);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.radDecrypt);
+            this.Controls.Add(this.radCrypt);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSourceFile);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnBrowse;
+    private System.Windows.Forms.TextBox txtSourceFile;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox txtPassword;
+    private System.Windows.Forms.RadioButton radCrypt;
+    private System.Windows.Forms.RadioButton radDecrypt;
+    private System.Windows.Forms.ProgressBar progressBar;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Button btnExit;
+  }
+}
+
---
+++ xorHW/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xorHW
+{
+  public partial class Form1 : Form
+  {
+        Thread thCrypt = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar.Minimum = 0;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+            dlg.Multiselect = false;
+            dlg.Title = "Открытия файла";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtSourceFile.Text = dlg.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (txtPassword != null)
+            {
+                try
+                {
+                    if (radCrypt.Enabled)
+                    {
+                        thCrypt = new Thread(ForEncrypt);
+                        thCrypt.IsBackground = true;
+                        thCrypt.Start();
+                    }
+                    else if (radDecrypt.Enabled)
+                    {
+                        thCrypt = new Thread(ForDecrypt);
+                        thCrypt.IsBackground = true;
+                        thCrypt.Start();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите пароль!");
+            }
+        }
+
+        public string Encrypt(string message, int key)
+        {
+            string result = "";
+            for (int i = 0; i < message.Length; i++)
+            result += (char)(message[i] ^ key);
+            return result;
+        }
+
+        public string Decrypt(string message, int key)
+        {
+            return Encrypt(message, key);
+        }
+
+        public void ForEncrypt()
+        {
+            string encryptedText = "";
+
+            using (FileStream fstream = File.OpenRead(txtSourceFile.Text))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(array);
+
+                encryptedText = Encrypt(textFromFile, Convert.ToInt32(txtPassword.Text));
+            }
+
+            using (FileStream fstream = new FileStream(txtSourceFile.Text, FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = Encoding.Default.GetBytes(encryptedText);
+
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+
+
+                progressBar.Invoke(new Action<int>(
+                    (x) =>
+                    {
+                        progressBar.Maximum = x;
+                        progressBar.Update();
+                    }), (int)fstream.Length);
+
+                progressBar.Invoke(new Action<int>(
+                    (x) =>
+                    {
+                        progressBar.Value = x;
+                        progressBar.Update();
+                    }), (int)fstream.Length);
+            }
+        }
+
+        public void ForDecrypt()
+        {
+            string decryptedText = "";
+
+            using (FileStream fstream = File.OpenRead(txtSourceFile.Text))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(array);
+
+                decryptedText = Decrypt(textFromFile, Convert.ToInt32(txtPassword.Text));
+            }
+
+            using (FileStream fstream = new FileStream(txtSourceFile.Text, FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = Encoding.Default.GetBytes(decryptedText);
+
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+
+                progressBar.Invoke(new Action<int>(
+                    (x) =>
+                    {
+                        progressBar.Maximum = x;
+                        progressBar.Update();
+                    }), (int)fstream.Length);
+
+                progressBar.Invoke(new Action<int>(
+                    (x) =>
+                    {
+                        progressBar.Value = x;
+                        progressBar.Update();
+                    }), (int)fstream.Length);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
